39bb1b8a68a380384bbc13fe3092e3b3e29f0a7d
wozog <hugowojtek@o2.pl>
2018-01-13 12:27:38 +0100

    init7

--- src/main/java/pl/sda/Main.java
+++ src/main/java/pl/sda/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("hello world2");
-        System.out.println("hello task23");
+        System.out.println("hello task234");
     }
 
 
