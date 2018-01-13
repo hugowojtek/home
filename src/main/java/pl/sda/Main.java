72e3e52561b9918162e59fa589eed3cba521c797
wozog <hugowojtek@o2.pl>
2018-01-13 13:10:20 +0100

    init11

--- src/main/java/pl/sda/Main.java
+++ src/main/java/pl/sda/Main.java
@@ -3,8 +3,8 @@ package pl.sda;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("hello world2");
-        System.out.println("hello task234");
+        System.out.println("master comeBack");
+
     }
 
 
